e8d0dd59b9e85d3743a4c6ac4e80a60ac30c6858
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-04-13 19:54:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "nex12" # Replace with your actual S3 bucket name
+    bucket = "new2311" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
-    region = "eu-north-1"
+    region = "us-east-1"
   }
 }
